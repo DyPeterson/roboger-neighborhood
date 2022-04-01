677aad00ef7d2079e7abd2a62d4b7ab66083e720
Dylan Peterson <dylan.peterson17@gmail.com>
2022-04-01 11:38:13 -0700

    change importance of numbers so that 3 is changed first then 2 then 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@
 function beepBoop(number) {
   const numberArray = [];
   for (let i = 0; i <= number; i ++) {
-    if (i.toString().includes(1)) {
-      numberArray[i] = "Beep!";
+    if (i.toString().includes(3)) {
+      numberArray[i] = "Won't you be my neighbor?";
     }
     else if (i.toString().includes(2)) {
       numberArray[i] = "Boop!";
     }
-    else if (i.toString().includes(3)){
-      numberArray[i] = "Won't you be my neighbor?"
+    else if (i.toString().includes(1)){
+      numberArray[i] = "Beep!";
     }
     else {
       numberArray[i] = i.toString();
