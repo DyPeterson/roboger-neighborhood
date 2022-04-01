4777e5250c5ac74d82e08eb822d3c58928436729
Dylan Peterson <dylan.peterson17@gmail.com>
2022-04-01 10:00:05 -0700

    WIP turn input into array of values

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
-function beepBoop(number)
-  
+function beepBoop(number) {
+  const numberArray = [];
+  for (let index = 0; index <= number; index ++) {
+    numberArray.push(index.toString());
+    console.log(number);
+  }
+}
